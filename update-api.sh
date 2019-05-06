d8e9d247a98e021566c42da983e183dcb04278b8
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-05-06 21:07:15 +0200

    update

--- update-api.sh
+++ update-api.sh
@@ -4,5 +4,5 @@ rm -rf gh-pages \
  && cp swagger.yml gh-pages \
  && ./gradlew clean gitChangelogTask build -i -Dhttp.socketTimeout=60000 -Dhttp.connectionTimeout=60000 \
  && rm -rf gh-pages/api && ./gradlew run --args="-af gh-pages/api" \
- && cd web && npm run build && cd .. \
+ && cd web && npm run build && cd .. && cp -rv --remove-destination web/dist/* gh-pages \
  && cd gh-pages && git add . && git commit -a -m "Updated" && git push
