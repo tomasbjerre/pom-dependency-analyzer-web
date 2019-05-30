8e2fd0b964ac02f053fa61a97a82a0b3d5f8d594
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-05-30 10:40:31 +0200

    doc

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 
 rm -rf gh-pages \
  && npm install pom-dependency-analyzer-web-frontend \
-
  && git clone git@github.com:tomasbjerre/pom-dependency-web.git -b gh-pages gh-pages \
  && rm -rfv gh-pages/* \
  && cp -rv node_modules/pom-dependency-analyzer-web-frontend/dist/* gh-pages \
-
  && npx pom-dependency-analyzer-web-api -af gh-pages/api \
-
  && cd gh-pages && git add . && git commit -a -m "Updated" && git push
